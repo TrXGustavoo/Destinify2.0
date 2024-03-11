342751b2ba69f23de19fec9f4eb5facd92b1c9a9
Gustavo Passos <gustavofp111@gmail.com>
2024-03-11 18:22:34 -0300

    Criando os principais templates

--- Destinify/settings.py
+++ Destinify/settings.py
@@ -104,9 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
-# STATIC_ROOT = os.path.join("static")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
+STATIC_ROOT = os.path.join("static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from main import views
+from django.contrib.auth.models import User
+
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('accounts/cadastro', views.cadastro, name='cadastro'),
+    path('accounts/login', views.login, name='login'),
+    path('accounts/logout', views.logout, name='logout'),
+    path('perfil/<int:user_id>/', views.perfil, name='perfil'),
+
+]
